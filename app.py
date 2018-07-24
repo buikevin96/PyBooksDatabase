fff78828b7ed1ccdb7459e2f0bce5853111ef046
buikevin96 <buikevin96@gmail.com>
2018-07-23 21:27:04 -0400

    Added and updated delete functionality

--- app.py
+++ app.py
@@ -45,6 +45,10 @@ def prompt_read_book():
 
     database.mark_book_as_read(name)
 
-# def prompt_read_book() ask for book name and change it to "read" in our list
+def prompt_delete_book():
+    name = input('Enter the name of the book you would like to delete: ')
+
+    database.delete_book(name)
+
 # def prompt_delete_book() ask for book name and remove book from list
 
--- utils/database.py
+++ utils/database.py
@@ -14,3 +14,12 @@ def mark_book_as_read(name):
     for book in books:
         if book['name'] == name:
             book['read'] = True
+
+def delete_book(name):
+    global books # Uses books that is not local
+    books_new = [book for book in books if book['name'] != 'name'] # Add each book to new list if book['name'] != name
+
+    for book in books:
+        if book['name'] == name:
+            books.remove(book)
+
