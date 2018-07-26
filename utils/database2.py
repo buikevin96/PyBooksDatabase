8ffdc50c285778763639622c60b772d1e795be86
buikevin96 <buikevin96@gmail.com>
2018-07-26 00:35:40 -0400

    Created new save_all_books function to allow program to save new read books list (mark_book_as_read update)

--- utils/database2.py
+++ utils/database2.py
@@ -11,7 +11,7 @@ books_file = 'books.txt'
 
 def add_book(name, author):
     with open('books_file', 'a') as file:
-        file.write(f'{name},{author},0')
+        file.write(f'{name},{author},0\n')
     books.append({'name': name, 'author': author, 'read': False})
 
 def get_all_books():
@@ -23,12 +23,20 @@ def get_all_books():
         for line in lines
     ]
 
-
-
 def mark_book_as_read(name):
+    books = get_all_books() # Read all the books
+    # Modify the book we want
     for book in books:
         if book['name'] == name:
-            book['read'] = True
+            book['read'] = '1'
+    _save_all_books(books) # Saves them all back
+
+# Opens file, delete all contents, goes through all of items in parameter, and types it in csv format
+def _save_all_books(books):
+    with open(books_file, 'w') as file:
+        for book in books:
+            file.write(f"{book['name']},{book['author']},{book['read']}\n")
+
 
 def delete_book(name):
     global books # Uses books that is not local
